b544ce77d76817780c9ce5c7d14aeb7b29930d0d
kcbj-xx <gabeandreas.avila@gmail.com>
2026-05-12 20:08:41 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// Handle notification clicks
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then((clientList) => {
+            for (const client of clientList) {
+                if (client.url === '/' && 'focus' in client) return client.focus();
+            }
+            if (clients.openWindow) return clients.openWindow('/');
+        })
+    );
+});
