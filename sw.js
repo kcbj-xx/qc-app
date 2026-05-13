3cbe7d94718f39e891bf81500848b714cdd80971
kcbj-xx <gabeandreas.avila@gmail.com>
2026-05-12 22:25:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,14 @@
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    // Bring the app to the foreground when clicking the notification
     event.waitUntil(
         clients.matchAll({type: 'window'}).then(windowClients => {
             for (var i = 0; i < windowClients.length; i++) {
                 var client = windowClients[i];
-                if (client.url === '/' && 'focus' in client) {
-                    return client.focus();
-                }
+                // Focus the app if it's already open
+                if ('focus' in client) return client.focus();
             }
-            if (clients.openWindow) {
-                return clients.openWindow('/');
-            }
+            // Open the app if it's closed
+            if (clients.openWindow) return clients.openWindow('/');
         })
     );
-});
-
-// This handles the background notification
-self.addEventListener('push', function(event) {
-    const options = {
-        body: 'Time to collect samples!',
-        vibrate: [500, 110, 500, 110, 450, 110, 200, 110, 170, 40, 450, 110, 200, 110, 170, 40],
-        requireInteraction: true,
-        tag: 'qc-sampling-alarm' // Prevents duplicate notifications
-    };
-    event.waitUntil(self.registration.showNotification('QC Alert', options));
 });
